2030b8b909a608e3086a3ce083bc9b25369270d9
13286378616 <13286378616@163.com>
2019-03-05 11:07:43 +0800

    修改合同类型

--- src/test/java/com/contract/web/cases2/Contract_type_management.java
+++ src/test/java/com/contract/web/cases2/Contract_type_management.java
@@ -1,5 +1,6 @@
 package com.contract.web.cases2;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -7,6 +8,7 @@ import com.contract.web.cases.Base4;
 import com.contract.web.util.AssertionUtil;
 
 public class Contract_type_management extends Base4{
+	private Logger logger = Logger.getLogger(Contract_type_management.class);
 	@Test
 	public void test1(){
 		
@@ -14,7 +16,8 @@ public class Contract_type_management extends Base4{
 		click(getElement("主页", "合同类型管理"));
 		click(getElement("合同类型管理页", "添加合同类型模板"));
 		String actualhetong = getElement(By.xpath("//span[text()='添加合同类型']")).getText();
-		AssertionUtil.assertTextEquals(actualhetong, "565656");
+		logger.info("-------------断言是否进入合同类型页面----------------");
+		AssertionUtil.assertTextEquals(actualhetong, "添加合同类型");
 		click(getElement("主页", "企业签章"));
 	}
 }
